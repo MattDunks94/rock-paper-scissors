6f75bd231c7a1982cae4e1f43b3fddf9a61c401a
Matt <mjd014@live.co.uk>
2022-07-07 18:34:33 +0000

    put all functions into one main function, modified and added print statements within the while loop

--- run.py
+++ run.py
@@ -4,9 +4,6 @@ from random import randint
 # Create player options list.
 player_options = ["rock", "paper", "scissors"]
 
-# Assign the opposition to a random list item.
-cpu = player_options[randint(0, 2)]
-
 
 def start_game():
     """
@@ -37,43 +34,60 @@ def main_game():
     cpu_score = 0
     player = False
 
+    # Assign the opposition to a random list item from player_options.
+    cpu = player_options[randint(0, 2)]
+
     while player == False:
 
-        player = input("\nRock, Paper, Scissors? ")
+        player = input("\nRock, Paper, Scissors?\n")
         if player == cpu:
-            print("\nIt's a draw!\n")
+            print(player, "VS", cpu, "\n")
+            print("It's a DRAW!\n")
         elif player == "rock":
             if cpu == "paper":
-                print("\nYou lose!", cpu, "covers", player, "\n")
+                print(player, "VS", cpu, "\n")
+                print("You LOSE!", cpu, "covers", player, "\n")
                 cpu_score += 1
             else:
-                print("\nYou win!", player, "crushes", cpu, "\n")
+                print(player, "VS", cpu, "\n")
+                print("You WIN!", player, "crushes", cpu, "\n")
                 player_score += 1
         elif player == "paper":
             if cpu == "scissors":
-                print("\nYou lose!", cpu, "slices", player, "\n")
+                print(player, "VS", cpu, "\n")
+                print("You LOSE!", cpu, "slices", player, "\n")
                 cpu_score += 1
             else:
-                print("\nYou win!", player, "covers", cpu, "\n")
+                print(player, "VS", cpu, "\n")
+                print("You WIN!", player, "covers", cpu, "\n")
                 player_score += 1
         elif player == "scissors":
             if cpu == "rock":
-                print("You lose!", cpu, "crushes", player, "\n")
+                print(player, "VS", cpu, "\n")
+                print("You LOSE!", cpu, "crushes", player, "\n")
                 cpu_score += 1
             else:
-                print("\nYou win!", player, "slices", cpu, "\n")
+                print(player, "VS", cpu, "\n")
+                print("You WIN!", player, "slices", cpu, "\n")
                 player_score += 1
         else:
             print("\nWe do not have that in our inventory!")
             print("Please choose 1 of the 3 options available.\n")
         player = False
+        cpu = player_options[randint(0, 2)]
 
         print("Your Score:", player_score)
         print("CPU Score:", cpu_score)
 
-                
+
+def main():
+    # All functions created into one main function.
+    start_game()
+    main_game()
+
+
 print("\n---------------------")
 print("ROCK, PAPER, SCISSORS")
 print("---------------------")
 
-start_game()
+main()
